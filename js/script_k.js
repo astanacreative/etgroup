74d61baada2b50790f5c5e24e7e2495e223e2e16
Kairat12 <56346130+Kairat12@users.noreply.github.com>
2022-03-19 23:16:36 +0600

    add slider

--- js/script_k.js
+++ js/script_k.js
@@ -1,12 +1,9 @@
-let calculate = document.getElementById("calculation");
 let count = document.getElementById("buttonCountNumber");
 
 document.getElementById("buttonCountPlus").onclick = function() {
-  let countPlus = count.innerHTML;
+  let countPlus = count.value;
   if(+countPlus <= 20){
     count.value++;
-    let countPlus = count.innerHTML;
-    calculate.innerHTML = calculations(countPlus) ;
   }
 }
 
@@ -14,13 +11,23 @@ document.getElementById("buttonCountMinus").onclick = function() {
   let countMinus = count.value;
   if(+countMinus >= 2){
     count.value--;
-    let countMinus = count.value;
-    calculate.innerHTML = calculations(countMinus) ;
   }
 }
-
-calculations = (count) => {
-  return count ;
-}
+const tabBody = document.querySelectorAll('.tab-item__body');
+tabBody.forEach((elem) => {
+	elem.addEventListener('mouseenter', function () {
+		elem.parentElement.classList.add('active');
+	});
+	elem.addEventListener('mouseleave', function () {
+		elem.parentElement.classList.remove('active');
+	});
+});
 
 $('.single-prod-slider').slick();
+
+
+$('.recomend-slider-block').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
